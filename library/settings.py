fa4e776b535667d547fc4d49a4da8ee7b509005e
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-06 14:18:27 +0200

    update urls

--- library/settings.py
+++ library/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
