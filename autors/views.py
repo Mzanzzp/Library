321588874565886019fcfa67e47198ae9c19e3de
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-06 14:17:33 +0200

    update urls

--- autors/views.py
+++ autors/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from autors.models import Autor
 
 
 def autors_list(request):
-    autors = Autor.objects.all()
+
     return render(
         request=request,
-        template_name="autor_list.html",
-        context={"autors": autors}
-
-    )
-
-def autor_description(request):
-    autors = Autor.object.all()
-    return render(
-        request=request,
-        template_name="autor_description.html",
-        context={"autors": autors}
+        template_name="autor_list.html"
 
     )
