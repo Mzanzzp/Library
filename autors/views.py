3951a5f6db3233821425eb57a3ee103c3f4b6179
zxf111 <mikolaj9812@gmail.com>
2022-08-06 14:22:54 +0200

    poprawka

--- autors/views.py
+++ autors/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from autors.models import Autor
 
 
 def autors_list(request):
@@ -8,5 +9,14 @@ def autors_list(request):
     return render(
         request=request,
         template_name="autor_list.html"
+
+    )
+
+def autor_description(request):
+    autors = Autor.object.all()
+    return render(
+        request=request,
+        template_name="autor_description.html",
+        context={"autors": autors}
 
     )
