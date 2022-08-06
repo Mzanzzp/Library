06b6f10cd4f5894af4b10d48e318e91c803fda6a
zxf111 <mikolaj9812@gmail.com>
2022-08-06 11:31:32 +0200

    models autor

--- autors/models.py
+++ autors/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    year_of_birth = models.DateField(null=True, blank=True)
