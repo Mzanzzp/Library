d6b63a794d0df3d95c24a8f575d244bf84c99fed
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-06 12:09:43 +0200

    adding book views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view(request):
+    return render(request=request,
+                  template_name='books.html'
+                  )
