828b12878d2068be4e70aa2c112570a2276903ea
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-06 12:27:59 +0200

    update book views.py

--- books/views.py
+++ books/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from books.models import Book
 
 
 def view(request):
+
+    books = Book.objects.all()
     return render(request=request,
-                  template_name='books.html'
+                  template_name='books.html',
+                  context={'books': books},
                   )
