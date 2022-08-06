78ae59eb4fa1cf4a07e4ad174285a02f0ac052af
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-06 11:36:33 +0200

    migrations

--- books/models.py
+++ books/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from autors.models import Autor
 
 
 class Book(models.Model):
-    pass
+    title = models.CharField(max_length=250)
+    relase_date = models.DateField(null=True, blank=True)
+    author = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Autor: {self.author} Title: {self.title}"
