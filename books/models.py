edeb5260cbcb8b7525641a4e474080734bc41f4d
bozlinski <bartlomiej.gozlinski@gmail.com>
2022-08-06 11:55:27 +0200

    Book models update

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from autors.models import Autor
 
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    relase_date = models.DateField(null=True, blank=True)
+    release_date = models.DateField(null=True, blank=True)
     author = models.ForeignKey(Autor, on_delete=models.CASCADE)
 
     def __str__(self):
