01a7bfc1a8c8706c6d4db41dc469f9a82a665e1c
zxf111 <mikolaj9812@gmail.com>
2022-08-06 11:49:31 +0200

    dodaje str do autor

--- autors/models.py
+++ autors/models.py
@@ -6,3 +6,6 @@ class Autor(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     year_of_birth = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.surname} {self.year_of_birth}"
